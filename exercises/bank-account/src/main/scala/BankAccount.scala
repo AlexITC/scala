75a8737596335e4bf643798969b16c2342c09ba9
Alexis Hernandez <alexis22229@gmail.com>
2018-03-04 16:48:16 -0600

    Solve the BankAccount exercise

--- exercises/bank-account/src/main/scala/BankAccount.scala
+++ exercises/bank-account/src/main/scala/BankAccount.scala
@@ -8,6 +8,38 @@ trait BankAccount {
 }
 
 object Bank {
-  def openAccount(): BankAccount = ???
+  def openAccount(): BankAccount = {
+    new CustomBankAccount
+  }
 }
 
+class CustomBankAccount extends BankAccount {
+
+  private val lock = new Object
+
+  private var balance = 0
+  private var isAvailable = true
+
+  override def closeAccount(): Unit = withLock {
+    isAvailable = false
+  }
+
+  override def getBalance: Option[Int] = withLock {
+    Option(isAvailable)
+      .filter(identity)
+      .map(_ => balance)
+  }
+
+  override def incrementBalance(increment: Int): Option[Int] = withLock {
+    val result = Option(isAvailable)
+      .filter(identity)
+      .map { _ =>
+        balance + increment
+      }
+
+    result.foreach { newBalance => balance = newBalance }
+    result
+  }
+
+  private def withLock[A](f: => A) = lock.synchronized(f)
+}
